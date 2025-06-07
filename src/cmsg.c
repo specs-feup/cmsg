5894f69867b4b90a53e0de9118f0bfa9e2afaa68
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2025-06-07 15:39:28 +0100

    Further implementation

---
+++ src/cmsg.c
@@ -0,0 +1,317 @@
+#include "cmsg.h"
+
+cmsg *
+cmsg_create ()
+{
+  cmsg *msg = (cmsg *)malloc (sizeof (cmsg));
+  msg->sbuf = (msgpack_sbuffer *)malloc (sizeof (msgpack_sbuffer));
+  msg->pk = (msgpack_packer *)malloc (sizeof (msgpack_packer));
+
+  msgpack_sbuffer_init (msg->sbuf);
+  msgpack_packer_init (msg->pk, msg->sbuf, msgpack_sbuffer_write);
+
+  return msg;
+}
+
+void
+cmsg_free (cmsg *msg)
+{
+  if (msg)
+    {
+      msgpack_sbuffer_destroy (msg->sbuf);
+
+      free (msg);
+    }
+}
+
+void
+cmsg_clear (cmsg *msg)
+{
+  if (msg)
+    msgpack_sbuffer_clear (msg->sbuf);
+}
+
+void
+cmsg_print (cmsg *msg)
+{
+  if (!msg)
+    return;
+
+  msgpack_unpacked result;
+  msgpack_unpacked_init (&result);
+
+  size_t offset = 0;
+
+  while (
+      msgpack_unpack_next (&result, msg->sbuf->data, msg->sbuf->size, &offset))
+    {
+      msgpack_object_print (stdout, result.data);
+    }
+
+  msgpack_unpacked_destroy (&result);
+}
+
+cmsg_err
+cmsg_insert_map (cmsg *msg, size_t entries)
+{
+  msgpack_pack_map (msg->pk, entries);
+
+  return CMSG_OK;
+}
+
+cmsg_err
+cmsg_insert_nested_map (cmsg *msg, char *key, size_t entries)
+{
+  msgpack_pack_str (msg->pk, strlen (key));
+  msgpack_pack_str_body (msg->pk, key, strlen (key));
+  msgpack_pack_map (msg->pk, entries);
+}
+
+cmsg_err
+cmsg_insert_pair_i8 (cmsg *msg, char *key, int8_t value)
+{
+  msgpack_pack_str (msg->pk, strlen (key));
+  msgpack_pack_str_body (msg->pk, key, strlen (key));
+  msgpack_pack_int8 (msg->pk, value);
+
+  return CMSG_OK;
+}
+
+cmsg_err
+cmsg_insert_pair_i16 (cmsg *msg, char *key, int16_t value)
+{
+  msgpack_pack_str (msg->pk, strlen (key));
+  msgpack_pack_str_body (msg->pk, key, strlen (key));
+  msgpack_pack_int16 (msg->pk, value);
+
+  return CMSG_OK;
+}
+
+cmsg_err
+cmsg_insert_pair_i32 (cmsg *msg, char *key, int32_t value)
+{
+  msgpack_pack_str (msg->pk, strlen (key));
+  msgpack_pack_str_body (msg->pk, key, strlen (key));
+  msgpack_pack_int32 (msg->pk, value);
+
+  return CMSG_OK;
+}
+
+cmsg_err
+cmsg_insert_pair_ui8 (cmsg *msg, char *key, uint8_t value)
+{
+  msgpack_pack_str (msg->pk, strlen (key));
+  msgpack_pack_str_body (msg->pk, key, strlen (key));
+  msgpack_pack_uint8 (msg->pk, value);
+
+  return CMSG_OK;
+}
+
+cmsg_err
+cmsg_insert_pair_ui16 (cmsg *msg, char *key, uint16_t value)
+{
+  msgpack_pack_str (msg->pk, strlen (key));
+  msgpack_pack_str_body (msg->pk, key, strlen (key));
+  msgpack_pack_uint16 (msg->pk, value);
+
+  return CMSG_OK;
+}
+
+cmsg_err
+cmsg_insert_pair_ui32 (cmsg *msg, char *key, uint32_t value)
+{
+  msgpack_pack_str (msg->pk, strlen (key));
+  msgpack_pack_str_body (msg->pk, key, strlen (key));
+  msgpack_pack_uint32 (msg->pk, value);
+
+  return CMSG_OK;
+}
+
+static msgpack_object *
+private_get_map_object_from_key(msgpack_object *obj, const char *key)
+{
+  if (obj->type != MSGPACK_OBJECT_MAP)
+    return NULL;
+
+  for (size_t i = 0; i < obj->via.map.size; ++i)
+    {
+      msgpack_object k = obj->via.map.ptr[i].key;
+      if (k.type == MSGPACK_OBJECT_STR &&
+          k.via.str.size == strlen(key) &&
+          strncmp(k.via.str.ptr, key, k.via.str.size) == 0)
+        return &(obj->via.map.ptr[i].val);
+    }
+
+  return NULL;
+}
+
+int8_t
+cmsg_get_i8_from_key (cmsg *msg, char *key, cmsg_err *err)
+{
+  if (!msg || !key || !err)
+    {
+      if (err)
+        *err = CMSG_ERR_INVALID;
+      return 0;
+    }
+
+  msgpack_unpacked result;
+  msgpack_unpacked_init (&result);
+  bool ok
+      = msgpack_unpack_next (&result, msg->sbuf->data, msg->sbuf->size, NULL);
+
+  if (!ok || result.data.type != MSGPACK_OBJECT_MAP)
+    {
+      *err = CMSG_ERR_PARSE;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  msgpack_object *val = private_get_map_object_from_key (&result.data, key);
+  if (!val)
+    {
+      *err = CMSG_ERR_KEY_NOT_FOUND;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  if (val->type != MSGPACK_OBJECT_POSITIVE_INTEGER
+      && val->type != MSGPACK_OBJECT_NEGATIVE_INTEGER)
+    {
+      *err = CMSG_ERR_TYPE_MISMATCH;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  int64_t raw_value = val->via.i64;
+  if (raw_value < INT8_MIN || raw_value > INT8_MAX)
+    {
+      *err = CMSG_ERR_OUT_OF_RANGE;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  *err = CMSG_OK;
+  msgpack_unpacked_destroy (&result);
+  return (int8_t)raw_value;
+}
+
+int16_t
+cmsg_get_i16_from_key (cmsg *msg, char *key, cmsg_err *err)
+{
+  if (!msg || !key || !err)
+    {
+      if (err)
+        *err = CMSG_ERR_INVALID;
+      return 0;
+    }
+
+  msgpack_unpacked result;
+  msgpack_unpacked_init (&result);
+  bool ok
+      = msgpack_unpack_next (&result, msg->sbuf->data, msg->sbuf->size, NULL);
+
+  if (!ok || result.data.type != MSGPACK_OBJECT_MAP)
+    {
+      *err = CMSG_ERR_PARSE;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  msgpack_object *val = private_get_map_object_from_key (&result.data, key);
+  if (!val)
+    {
+      *err = CMSG_ERR_KEY_NOT_FOUND;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  if (val->type != MSGPACK_OBJECT_POSITIVE_INTEGER
+      && val->type != MSGPACK_OBJECT_NEGATIVE_INTEGER)
+    {
+      *err = CMSG_ERR_TYPE_MISMATCH;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  int64_t raw_value = val->via.i64;
+  if (raw_value < INT16_MIN || raw_value > INT16_MAX)
+    {
+      *err = CMSG_ERR_OUT_OF_RANGE;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  *err = CMSG_OK;
+  msgpack_unpacked_destroy (&result);
+  return (int16_t)raw_value;
+}
+
+int32_t
+cmsg_get_i32_from_key (cmsg *msg, char *key, cmsg_err *err)
+{
+  if (!msg || !key || !err)
+    {
+      if (err)
+        *err = CMSG_ERR_INVALID;
+      return 0;
+    }
+
+  msgpack_unpacked result;
+  msgpack_unpacked_init (&result);
+  bool ok
+      = msgpack_unpack_next (&result, msg->sbuf->data, msg->sbuf->size, NULL);
+
+  if (!ok || result.data.type != MSGPACK_OBJECT_MAP)
+    {
+      *err = CMSG_ERR_PARSE;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  msgpack_object *val = private_get_map_object_from_key (&result.data, key);
+  if (!val)
+    {
+      *err = CMSG_ERR_KEY_NOT_FOUND;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  if (val->type != MSGPACK_OBJECT_POSITIVE_INTEGER
+      && val->type != MSGPACK_OBJECT_NEGATIVE_INTEGER)
+    {
+      *err = CMSG_ERR_TYPE_MISMATCH;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  int64_t raw_value = val->via.i64;
+  if (raw_value < INT32_MIN || raw_value > INT32_MAX)
+    {
+      *err = CMSG_ERR_OUT_OF_RANGE;
+      msgpack_unpacked_destroy (&result);
+      return 0;
+    }
+
+  *err = CMSG_OK;
+  msgpack_unpacked_destroy (&result);
+  return (int32_t)raw_value;
+}
+
+uint8_t
+cmsg_get_ui8_from_key (cmsg *msg, char *key, cmsg_err *err)
+{
+  return 0;
+}
+
+uint16_t
+cmsg_get_ui16_from_key (cmsg *msg, char *key, cmsg_err *err)
+{
+  return 0;
+}
+
+uint32_t
+cmsg_get_ui32_from_key (cmsg *msg, char *key, cmsg_err *err)
+{
+  return 0;
+}
--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+#include "cmsg.h"
+
 int
 main (void)
 {
-  printf ("hello world\n");
+  cmsg *msg = cmsg_create ();
+
+  cmsg_insert_map(msg, 2);
+  cmsg_insert_pair_i32 (msg, "value1", 10);
+  cmsg_insert_pair_i32 (msg, "value2", 10);
+
+  cmsg_err err;
+  int32_t v1 = cmsg_get_i32_from_key(msg, "value1", &err);
+
+  printf("v1: %d\n", v1);
+
+  cmsg_print (msg);
+
+  cmsg_free (msg);
 
   return 0;
 }
